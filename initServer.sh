bc602b51141bf4bae591f2d8a86ef6ff8716a2e1
minhee.son <minhee4735@gmail.com>
2022-10-12 17:46:43 +0900

    refactor 배포 스크립트 디렉토리 위치 변경

--- initServer.sh
+++ initServer.sh
@@ -8,5 +8,5 @@ echo -e ">>>>> 모든 docker 컨테이너를 시작합니다. "
 docker start $(docker ps -a -q)
 
 echo -e ">>>>>  java application 을 실행합니다. "
-java -jar -Dspring.profiles.active=prod /home/ubuntu/nextstep/monitoring/build/libs/subway-0.0.1-SNAPSHOT.jar &
+java -jar -Dspring.profiles.active=prod ./build/libs/subway-0.0.1-SNAPSHOT.jar &
 
